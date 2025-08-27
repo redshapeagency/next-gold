bb207d3b2395dc71fa5d367ef619d4ee4eb1e076
REDSHAPE <151862140+redshapeagency@users.noreply.github.com>
2025-08-27 20:26:32 +0200

    v.4

--- install.sh
+++ install.sh
@@ -169,7 +169,12 @@ install_redis() {
     sudo apt install -y redis
     
     # Configure Redis
-    sudo sed -i 's/# requirepass foobared/requirepass '${REDIS_PASSWORD}'/' /etc/redis/redis.conf
+echo "[$TIMESTAMP] Configuring Redis..."
+sudo sed -i 's/# requirepass foobared/requirepass '"$(printf '%s
+' "$REDIS_PASSWORD" | sed 's/[[\.*^$()+?{|]/\&/g')"'/' /etc/redis/redis.conf
+sudo sed -i 's/bind 127.0.0.1 ::1/bind 127.0.0.1/' /etc/redis/redis.conf
+sudo systemctl restart redis-server
+sudo systemctl enable redis-server${REDIS_PASSWORD}'/' /etc/redis/redis.conf
     sudo sed -i 's/# maxmemory <bytes>/maxmemory 256mb/' /etc/redis/redis.conf
     sudo sed -i 's/# maxmemory-policy noeviction/maxmemory-policy allkeys-lru/' /etc/redis/redis.conf
     
